8d14110b2a43b735c732af51aa3d69bc6343d4dc
ismetcanbyk <ismetcanbyk@gmail.com>
2022-10-22 20:56:53 +0300

    Patika sql ödevleri

--- ödev1.sql
+++ ödev1.sql
@@ -23,6 +23,8 @@ WHERE first_name = 'Mary';
 
 -- 5)film tablosundaki uzunluğu(length) 50 ten büyük OLMAYIP aynı zamanda rental_rate değeri 2.99 veya 4.99 OLMAYAN verileri sıralayınız.
 
+SELECT * FROM film 
+WHERE not length>50 and not (rental_rate = 2.99 or rental_rate = 4.99);
 
 
 
--- ödev3.sql
+++ ödev3.sql
@@ -18,3 +18,5 @@ WHERE title ILIKE '%t%t%t%t';
 
 -- 4)film tablosunda bulunan tüm sütunlardaki verilerden title 'C' karakteri ile başlayan ve uzunluğu (length) 90 dan büyük olan ve rental_rate 2.99 olan verileri sıralayınız.
 
+SELECT * FROM film
+WHERE title LIKE 'C%' AND length >90 AND rental_rate IN(2.99);

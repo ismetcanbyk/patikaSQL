fb908204cb34ab873fb625410fbf06f210023020
ismetcanbyk <ismetcanbyk@gmail.com>
2022-11-22 11:07:17 +0300

    Patika sql ödev 10

--- ödev10.sql
+++ ödev10.sql
@@ -11,3 +11,6 @@ RIGHT JOIN payment ON customer.customer_id = payment.customer_id;
 
 
 -- 3)customer tablosu ile rental tablosunda bulunan rental_id ile customer tablosundaki first_name ve last_name isimlerini birlikte görebileceğimiz FULL JOIN sorgusunu yazınız.
+
+SELECT rental.rental_id, customer.first_name, customer.last_name FROM customer 
+FULL JOIN rental ON customer.customer_id = rental.customer_id;
